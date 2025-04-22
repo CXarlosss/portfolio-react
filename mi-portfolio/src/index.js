2128416fc0935fa92cec94f733ff5b6a94f7b927
Carlos <carlosdepet@gmail.com>
2025-04-22 17:11:32 +0200

    CSS Mejor

--- mi-portfolio/src/index.js
+++ mi-portfolio/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.module.css'; // Optional global styles
+import "../src/styles/global.css"; // Optional global styles
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
