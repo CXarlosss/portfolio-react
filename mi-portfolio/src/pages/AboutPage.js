eca44c6e6f22273ce22dfb3e3b47b9a5d2ed00c7
Carlos <carlosdepet@gmail.com>
2025-04-29 11:41:29 +0200

    About-me Completado

--- mi-portfolio/src/pages/AboutPage.js
+++ mi-portfolio/src/pages/AboutPage.js
@@ -1,36 +1,37 @@
 // @ts-nocheck
-import React from 'react';
-import Introduction from '../components/About/Introduction';
-import SkillsSection from '../components/About/SkillsSection';
-import ExperienceSection from '../components/About/ExperienceSection';
-import EducationSection from '../components/About/EducationSection';
-import InterestsSection from '../components/About/InterestsSection';
-import styles from '../styles/pages/AboutPage.module.css'; // Create AboutPage.module.css
-
+import React from "react";
+import Introduction from "../components/About/Introduction";
+import SkillsSection from "../components/About/SkillsSection";
+import ExperienceSection from "../components/About/ExperienceSection";
+import EducationSection from "../components/About/EducationSection";
+import InterestsSection from "../components/About/InterestsSection";
+import styles from "../styles/pages/AboutPage.module.css"; // Create AboutPage.module.css
 
 function AboutPage() {
   return (
     <div className={styles.aboutPage}>
       <main className={styles.aboutMain}>
-    <div className={styles.introduction}>
-      <Introduction />
-    </div>
-    <div className={styles.secondarySection}>
-      <div className={styles.skillsSection}>
-        <SkillsSection />
-      </div>
-      <div className={styles.educationSection}>
-        <EducationSection />
-      </div>
-      <div className={styles.experienceSection}>
-        <ExperienceSection />
-      </div>
-    </div>
-    <div className={styles.interestsSection}>
-      <InterestsSection />
-    </div>
-</main>
+        <div className={styles.introduction}>
+          <Introduction />
+        </div>
 
+        <div className={styles.skillsSection}>
+          <SkillsSection />
+        </div>
+
+        <div className={styles.secondarySection}>
+          <div className={styles.educationSection}>
+            <EducationSection />
+          </div>
+          <div className={styles.experienceSection}>
+            <ExperienceSection />
+          </div>
+        </div>
+
+        <div className={styles.interestsSection}>
+          <InterestsSection />
+        </div>
+      </main>
     </div>
   );
 }
